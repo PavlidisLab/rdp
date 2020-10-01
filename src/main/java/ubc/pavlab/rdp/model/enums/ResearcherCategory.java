95f3bb914424be807d1c742429ef33ab82600df5
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2020-10-01 13:45:01 -0700

    Add a new researcher category for other

--- src/main/java/ubc/pavlab/rdp/model/enums/ResearcherCategory.java
+++ src/main/java/ubc/pavlab/rdp/model/enums/ResearcherCategory.java
@@ -10,7 +10,8 @@ public enum ResearcherCategory {
     IN_VITRO_BIOCHEMICAL( "in-vitro", "In vitro (biochemical)" ),
     IN_VITRO_CELLS( "in-vitro-cells", "In vitro (cells)" ),
     IN_VITRO_STRUCTURAL( "in-vitro-structural", "In vitro (structural)" ),
-    IN_VIVO( "in-vivo", "In vivo" );
+    IN_VIVO( "in-vivo", "In vivo" ),
+    OTHER( "other", "Other" );
     private String id;
     private String label;
 }
