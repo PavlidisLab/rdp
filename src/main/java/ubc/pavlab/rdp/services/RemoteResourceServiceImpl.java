1e259274d59b5165368737a19f73f40b62bdf3a2
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2020-10-08 11:11:18 -0700

    remote: Use UriComponentBuilder and URI to query remotes

--- src/main/java/ubc/pavlab/rdp/services/RemoteResourceServiceImpl.java
+++ src/main/java/ubc/pavlab/rdp/services/RemoteResourceServiceImpl.java
@@ -4,9 +4,11 @@ import lombok.extern.apachecommons.CommonsLog;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.util.UriComponentsBuilder;
 import ubc.pavlab.rdp.exception.RemoteException;
 import ubc.pavlab.rdp.model.Taxon;
 import ubc.pavlab.rdp.model.User;
@@ -17,8 +19,7 @@ import ubc.pavlab.rdp.repositories.RoleRepository;
 import ubc.pavlab.rdp.settings.ApplicationSettings;
 
 import javax.ws.rs.core.Response;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
+import java.net.URI;
 import java.text.MessageFormat;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -28,7 +29,7 @@ import java.util.stream.Collectors;
 public class RemoteResourceServiceImpl implements RemoteResourceService {
 
     private static final String API_USERS_SEARCH_URI = "/api/users/search";
-    private static final String API_USER_GET_URI = "/api/users/%s";
+    private static final String API_USER_GET_URI = "/api/users/{userId}";
     private static final String API_GENES_SEARCH_URI = "/api/genes/search";
 
     @Autowired
@@ -45,43 +46,45 @@ public class RemoteResourceServiceImpl implements RemoteResourceService {
 
     @Override
     public Collection<User> findUsersByLikeName( String nameLike, Boolean prefix, Optional<Collection<ResearcherCategory>> researcherTypes, Optional<Collection<String>> organUberonIds ) throws RemoteException {
-        return getRemoteEntities( User[].class, API_USERS_SEARCH_URI, new LinkedMultiValueMap<String, String>() {{
-            add( "nameLike", nameLike );
-            add( "prefix", prefix.toString() );
-            for ( String organUberonId : organUberonIds.orElse( Collections.emptySet() ) ) {
-                add( "organUberonIds", organUberonId );
-            }
-        }} );
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add( "nameLike", nameLike );
+        params.add( "prefix", prefix.toString() );
+        for ( String organUberonId : organUberonIds.orElse( Collections.emptySet() ) ) {
+            params.add( "organUberonIds", organUberonId );
+        }
+        addAuthParamIfAdmin( params );
+        return getRemoteEntities( User[].class, API_USERS_SEARCH_URI, params );
     }
 
     @Override
     public Collection<User> findUsersByDescription( String descriptionLike, Optional<Collection<ResearcherCategory>> researcherTypes, Optional<Collection<String>> organUberonIds ) throws RemoteException {
-        return getRemoteEntities( User[].class, API_USERS_SEARCH_URI, new LinkedMultiValueMap<String, String>() {{
-            add( "descriptionLike", descriptionLike );
-            for ( String organUberonId : organUberonIds.orElse( Collections.emptySet() ) ) {
-                add( "organUberonIds", organUberonId );
-            }
-        }} );
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add( "descriptionLike", descriptionLike );
+        for ( String organUberonId : organUberonIds.orElse( Collections.emptySet() ) ) {
+            params.add( "organUberonIds", organUberonId );
+        }
+        addAuthParamIfAdmin( params );
+        return getRemoteEntities( User[].class, API_USERS_SEARCH_URI, params );
     }
 
     @Override
     public Collection<UserGene> findGenesBySymbol( String symbol, Taxon taxon, Set<TierType> tiers, Integer orthologTaxonId, Optional<Collection<ResearcherCategory>> researcherTypes, Optional<Collection<String>> organUberonIds )
             throws RemoteException {
         List<UserGene> intlUsergenes = new LinkedList<>();
-        // TODO: use the tiers field for the v1.4 API
         for ( TierType tier : restrictTiers( tiers ) ) {
-            intlUsergenes.addAll(
-                    getRemoteEntities( UserGene[].class, API_GENES_SEARCH_URI, new LinkedMultiValueMap<String, String>() {{
-                        add( "symbol", symbol );
-                        add( "taxonId", taxon.getId().toString() );
-                        add( "tier", tier.toString() );
-                        if ( orthologTaxonId != null ) {
-                            add( "orthologTaxonId", orthologTaxonId.toString() );
-                        }
-                        for ( String organUberonId : organUberonIds.orElse( Collections.emptySet() ) ) {
-                            add( "organUberonIds", organUberonId );
-                        }
-                    }} ) );
+            MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+            params.add( "symbol", symbol );
+            params.add( "taxonId", taxon.getId().toString() );
+            // TODO: use the tiers field for the v1.4 API
+            params.add( "tier", tier.toString() );
+            if ( orthologTaxonId != null ) {
+                params.add( "orthologTaxonId", orthologTaxonId.toString() );
+            }
+            for ( String organUberonId : organUberonIds.orElse( Collections.emptySet() ) ) {
+                params.add( "organUberonIds", organUberonId );
+            }
+            addAuthParamIfAdmin( params );
+            intlUsergenes.addAll( getRemoteEntities( UserGene[].class, API_GENES_SEARCH_URI, params ) );
         }
         // add back-reference to user
         intlUsergenes.forEach( g -> g.setUser( g.getRemoteUser() ) );
@@ -90,28 +93,41 @@ public class RemoteResourceServiceImpl implements RemoteResourceService {
 
     @Override
     public User getRemoteUser( Integer userId, String remoteHost ) throws RemoteException {
-        // Check that the remoteHost is one of our known APIs and call it if it is.
-        if ( Arrays.stream( applicationSettings.getIsearch().getApis() ).anyMatch( remoteHost::equals ) ) {
-            User user = remoteRequest( User.class, remoteHost + String.format( API_USER_GET_URI, userId ),
-                    addAuthParamIfAdmin( new LinkedMultiValueMap<>() ) );
-            // add back-references to the user
-            user.getUserGenes().values().forEach( ug -> ug.setUser( user ) );
-            user.getUserTerms().forEach( ug -> ug.setUser( user ) );
-            user.getUserOrgans().values().forEach( ug -> ug.setUser( user ) );
-            return user;
-        } else {
+        // Check that the remoteHost is one of our known APIs
+        if ( Arrays.stream( applicationSettings.getIsearch().getApis() ).noneMatch( remoteHost::equals ) ) {
             throw new RemoteException( MessageFormat.format( "Unknown remote API {0}.", remoteHost ) );
         }
+
+        MultiValueMap queryParams = new LinkedMultiValueMap<String, String>();
+        addAuthParamIfAdmin( queryParams );
+        URI uri = UriComponentsBuilder.fromUriString( remoteHost )
+                .path( API_USER_GET_URI )
+                .queryParams( queryParams )
+                .buildAndExpand( Collections.singletonMap( "userId", userId ) )
+                .toUri();
+
+        User user = remoteRequest( User.class, uri );
+
+        // add back-references to the user
+        user.getUserGenes().values().forEach( ug -> ug.setUser( user ) );
+        user.getUserTerms().forEach( ug -> ug.setUser( user ) );
+        user.getUserOrgans().values().forEach( ug -> ug.setUser( user ) );
+
+        return user;
     }
 
-    private <T> Collection<T> getRemoteEntities( Class<T[]> arrCls, String uri, MultiValueMap<String, String> args )
+    private <T> Collection<T> getRemoteEntities( Class<T[]> arrCls, String path, MultiValueMap<String, String> params )
             throws RemoteException {
         Collection<T> entities = new LinkedList<>();
 
         // Call all APIs
         for ( String api : applicationSettings.getIsearch().getApis() ) {
             try {
-                entities.addAll( Arrays.asList( remoteRequest( arrCls, api + uri, addAuthParamIfAdmin( args ) ) ) );
+                URI uri = UriComponentsBuilder.fromUriString( api )
+                        .path( path )
+                        .queryParams( params )
+                        .build().toUri();
+                entities.addAll( Arrays.asList( remoteRequest( arrCls, uri ) ) );
             } catch ( RemoteException e ) {
                 log.error( MessageFormat.format( "Received error from remote API {0}:", api ), e );
             }
@@ -120,35 +136,14 @@ public class RemoteResourceServiceImpl implements RemoteResourceService {
         return entities;
     }
 
-    private <T> T remoteRequest( Class<T> cls, String remoteUrl, MultiValueMap<String, String> args ) throws RemoteException {
-        ResteasyWebTarget target = client.target( remoteUrl + encodeParams( args ) );
+    private <T> T remoteRequest( Class<T> cls, URI uri ) throws RemoteException {
+        ResteasyWebTarget target = client.target( uri );
         Response response = target.request().get();
         if ( response.getStatus() != 200 ) {
-            throw new RemoteException( "No data received: " + response.readEntity( String.class ) );
+            throw new RemoteException( MessageFormat.format( "No data received: {0}.", response.readEntity( String.class ) ) );
         }
 
         return response.readEntity( cls );
-    }
-
-    private String encodeParams( MultiValueMap<String, String> args ) {
-        StringBuilder s = new StringBuilder( "?" );
-        boolean first = true;
-        for ( String arg : args.keySet() ) {
-            if ( !first ) {
-                s.append( "&" );
-            } else {
-                first = false;
-            }
-            try {
-                for ( String a : args.get( arg ) ) {
-                    s.append( arg ).append( "=" ).append( URLEncoder.encode( a, "UTF8" ) );
-                }
-            } catch ( UnsupportedEncodingException e ) {
-                log.error( e );
-                e.printStackTrace();
-            }
-        }
-        return s.toString();
     }
 
     private Set<TierType> restrictTiers( Set<TierType> tiers ) {
@@ -157,12 +152,11 @@ public class RemoteResourceServiceImpl implements RemoteResourceService {
                 .collect( Collectors.toSet() );
     }
 
-    private MultiValueMap<String, String> addAuthParamIfAdmin( MultiValueMap<String, String> args ) {
+    private void addAuthParamIfAdmin( MultiValueMap<String, String> query ) {
         User user = userService.findCurrentUser();
         if ( user != null && user.getRoles().contains( roleRepository.findByRole( "ROLE_ADMIN" ) ) ) {
-            args.add( "auth", applicationSettings.getIsearch().getSearchToken() );
+            query.add( "auth", applicationSettings.getIsearch().getSearchToken() );
         }
-        return args;
     }
 
 }
--- src/test/java/ubc/pavlab/rdp/services/RemoteResourceServiceTest.java
+++ src/test/java/ubc/pavlab/rdp/services/RemoteResourceServiceTest.java
@@ -10,8 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import ubc.pavlab.rdp.exception.RemoteException;
+import ubc.pavlab.rdp.model.Role;
 import ubc.pavlab.rdp.model.Taxon;
 import ubc.pavlab.rdp.model.User;
 import ubc.pavlab.rdp.model.UserGene;
@@ -21,14 +23,15 @@ import ubc.pavlab.rdp.settings.ApplicationSettings;
 
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.EnumSet;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.*;
-import static ubc.pavlab.rdp.util.TestUtils.createTaxon;
-import static ubc.pavlab.rdp.util.TestUtils.createUser;
+import static ubc.pavlab.rdp.util.TestUtils.*;
 
 @RunWith(SpringRunner.class)
 public class RemoteResourceServiceTest {
@@ -72,41 +75,78 @@ public class RemoteResourceServiceTest {
         Invocation.Builder builder = mock( Invocation.Builder.class );
         when( builder.get() ).thenReturn( response );
         when( resteasyWebTarget.request() ).thenReturn( builder );
-        when( resteasyClient.target( any( String.class ) ) ).thenReturn( resteasyWebTarget );
+        when( resteasyClient.target( any( URI.class ) ) ).thenReturn( resteasyWebTarget );
     }
 
     @Test
-    public void findUserByLikeName_thenReturnSuccess() {
+    public void findUserByLikeName_thenReturnSuccess() throws URISyntaxException, RemoteException {
         when( response.getStatus() ).thenReturn( 200 );
         when( response.readEntity( User[].class ) ).thenReturn( new User[]{} );
         remoteResourceService.findUsersByLikeName( "ok", true, Optional.empty(), Optional.empty() );
-        verify( resteasyClient ).target( "example.com/api/users/search?nameLike=ok&prefix=true" );
+        verify( resteasyClient ).target( new URI( "example.com/api/users/search?nameLike=ok&prefix=true" ) );
     }
 
     @Test
-    public void findGenesBySymbol_whenTier3_isRestricted() {
+    @WithMockUser(roles = { "ADMIN" })
+    public void findUserByLikeName_whenAdmin_thenReturnSuccess() throws URISyntaxException, RemoteException {
+        Role adminRole = createRole( 1, "ROLE_ADMIN" );
+        User adminUser = createUserWithRoles( 1, adminRole );
+        when( iSearchSettings.getSearchToken() ).thenReturn( "1234" );
+        when( roleRepository.findByRole( "ROLE_ADMIN" ) ).thenReturn( adminRole );
+        when( userService.findCurrentUser() ).thenReturn( adminUser );
+        when( response.getStatus() ).thenReturn( 200 );
+        when( response.readEntity( User[].class ) ).thenReturn( new User[]{} );
+        remoteResourceService.findUsersByLikeName( "ok", true, Optional.empty(), Optional.empty() );
+        verify( resteasyClient ).target( new URI( "example.com/api/users/search?nameLike=ok&prefix=true&auth=1234" ) );
+    }
+
+    @Test
+    public void findGenesBySymbol_whenTier3_isRestricted() throws URISyntaxException, RemoteException {
         when( response.getStatus() ).thenReturn( 200 );
         when( response.readEntity( UserGene[].class ) ).thenReturn( new UserGene[]{} );
 
         Taxon taxon = createTaxon( 9606 );
         remoteResourceService.findGenesBySymbol( "ok", taxon, EnumSet.of( TierType.TIER1, TierType.TIER2, TierType.TIER3 ), null, Optional.empty(), Optional.empty() );
-        verify( resteasyClient ).target( "example.com/api/genes/search?symbol=ok&taxonId=9606&tier=TIER1" );
-        verify( resteasyClient ).target( "example.com/api/genes/search?symbol=ok&taxonId=9606&tier=TIER2" );
+        verify( resteasyClient ).target( new URI( "example.com/api/genes/search?symbol=ok&taxonId=9606&tier=TIER1" ) );
+        verify( resteasyClient ).target( new URI( "example.com/api/genes/search?symbol=ok&taxonId=9606&tier=TIER2" ) );
         verifyNoMoreInteractions( resteasyClient );
     }
 
     @Test
-    public void getRemoteUser_whenRemoteDoesNotExist_thenReturnSuccess() {
+    public void getRemoteUser_whenRemoteDoesNotExist_thenReturnSuccess() throws URISyntaxException, RemoteException {
         User user = createUser( 1 );
         when( response.getStatus() ).thenReturn( 200 );
         when( response.readEntity( User.class ) ).thenReturn( user );
         when( iSearchSettings.getApis() ).thenReturn( new String[]{ "example.com" } );
         User remoteUser = remoteResourceService.getRemoteUser( 1, "example.com" );
+        verify( resteasyClient ).target( new URI( "example.com/api/users/1" ) );
+        assertThat( remoteUser ).isEqualTo( user );
+    }
+
+    @Test
+    public void getRemoteUser_whenRemoteHostUriContainsPath_thenRelativizeWithEndpoint_thenReturnSuccess() throws URISyntaxException, RemoteException {
+        User user = createUser( 1 );
+        when( response.getStatus() ).thenReturn( 200 );
+        when( response.readEntity( User.class ) ).thenReturn( user );
+        when( iSearchSettings.getApis() ).thenReturn( new String[]{ "https://example.com/rgr" } );
+        User remoteUser = remoteResourceService.getRemoteUser( 1, "https://example.com/rgr" );
+        verify( resteasyClient ).target( new URI( "https://example.com/rgr/api/users/1" ) );
+        assertThat( remoteUser ).isEqualTo( user );
+    }
+
+    @Test
+    public void getRemoteUser_whenRemoteHostUriContainsPath_thenReplaceWithEndpoint_thenReturnSuccess() throws URISyntaxException, RemoteException {
+        User user = createUser( 1 );
+        when( response.getStatus() ).thenReturn( 200 );
+        when( response.readEntity( User.class ) ).thenReturn( user );
+        when( iSearchSettings.getApis() ).thenReturn( new String[]{ "https://example.com/" } );
+        User remoteUser = remoteResourceService.getRemoteUser( 1, "https://example.com/" );
+        verify( resteasyClient ).target( new URI( "https://example.com/api/users/1" ) );
         assertThat( remoteUser ).isEqualTo( user );
     }
 
     @Test(expected = RemoteException.class)
-    public void getRemoteUser_whenRemoteDoesNotExist_thenRaiseException() {
+    public void getRemoteUser_whenRemoteDoesNotExist_thenRaiseException() throws RemoteException {
         when( iSearchSettings.getApis() ).thenReturn( new String[]{ "example.com" } );
         remoteResourceService.getRemoteUser( 1, "example1.com" );
     }
