186037425b616abdfedefe448861f191888cb18f
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2021-08-04 14:52:29 -0700

    Add title, contact and description to the OpenAPI spec (fix #93)

--- src/main/java/ubc/pavlab/rdp/ApiConfig.java
+++ src/main/java/ubc/pavlab/rdp/ApiConfig.java
@@ -1,9 +1,11 @@
 package ubc.pavlab.rdp;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ubc.pavlab.rdp.controllers.ApiController;
@@ -11,6 +13,7 @@ import ubc.pavlab.rdp.settings.ApplicationSettings;
 import ubc.pavlab.rdp.settings.SiteSettings;
 
 import java.util.Collections;
+import java.util.Locale;
 
 /**
  * Configuration for the public search API.
@@ -22,9 +25,16 @@ public class ApiConfig {
     private SiteSettings siteSettings;
 
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI openAPI( MessageSource messageSource ) {
+        // FIXME: retrieve that from the request context
+        Locale locale = Locale.getDefault();
+        String shortname = messageSource.getMessage( "rdp.site.shortname", null, locale );
         return new OpenAPI()
-                .info( new Info().version( ApiController.API_VERSION ) )
+                .info( new Info()
+                        .title( messageSource.getMessage( "ApiConfig.title", new String[]{ shortname }, locale ) )
+                        .description( messageSource.getMessage( "ApiConfig.description", new String[]{ shortname }, locale ) )
+                        .contact( new Contact().email( siteSettings.getContactEmail() ) )
+                        .version( ApiController.API_VERSION ) )
                 .servers( Collections.singletonList( new Server().url( siteSettings.getHostUri().toString() ) ) );
 
     }
