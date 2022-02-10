7b90e9e4dc5829dfe8c0016dd121bea15de8de14
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2022-02-10 13:48:40 -0800

    Fix researcher category test failure

--- src/test/java/ubc/pavlab/rdp/repositories/UserRepositoryTest.java
+++ src/test/java/ubc/pavlab/rdp/repositories/UserRepositoryTest.java
@@ -479,7 +479,7 @@ public class UserRepositoryTest {
         user.getProfile().getResearcherCategories().add( ResearcherCategory.IN_VIVO );
         user = entityManager.persistAndFlush( user );
         assertThat( user.getProfile().getResearcherCategories() )
-                .containsExactly( ResearcherCategory.IN_SILICO, ResearcherCategory.IN_VIVO );
+                .containsExactlyInAnyOrder( ResearcherCategory.IN_SILICO, ResearcherCategory.IN_VIVO );
     }
 
     @Test
