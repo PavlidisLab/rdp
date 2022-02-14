47d7751104c73acefbd66a70e08c484afc0ee55f
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2022-02-14 10:44:36 -0800

    Fix warnings fom SonarQube
    Remove needless boolean boxing when checking if a user can update some
    content.
    
    Use == on enum instances.

--- src/main/java/ubc/pavlab/rdp/services/PrivacyServiceImpl.java
+++ src/main/java/ubc/pavlab/rdp/services/PrivacyServiceImpl.java
@@ -73,7 +73,7 @@ public class PrivacyServiceImpl implements PrivacyService {
     @Override
     public boolean checkUserCanUpdate( User user, UserContent userContent ) {
         // only admins or rightful owner can update user content
-        return user.getRoles().contains( getAdminRole() ) || userContent.getOwner().map( u -> u.equals( user ) ).orElse( false );
+        return user.getRoles().contains( getAdminRole() ) || userContent.getOwner().filter( user::equals ).isPresent();
     }
 
     private boolean checkUserCanSeeOtherUserContentWithPrivacyLevel( User currentUser, User otherUser, PrivacyLevelType privacyLevel ) {
@@ -98,10 +98,10 @@ public class PrivacyServiceImpl implements PrivacyService {
         // Either the user is looking at himself, or the user is public, or shared with registered users - check for any logged-in user, or private - check for admin; If logged-in user is admin, we have to
         // check whether this user is the designated actor for the authenticated remote search, in which case we have to check for remote search privileges on the user.
         return otherUser.equals( currentUser ) // User is looking at himself
-                || ( privacyLevel.equals( PrivacyLevelType.PUBLIC ) ) // Data is public
-                || ( privacyLevel.equals( PrivacyLevelType.SHARED ) && currentUser != null && !isRemoteSearchUser( currentUser ) )// data is accessible for registerd users and there is a user logged in who is not the remote admin
-                || ( privacyLevel.equals( PrivacyLevelType.PRIVATE ) && currentUser != null && currentUser.getRoles().contains( getAdminRole() ) && !isRemoteSearchUser( currentUser ) )// data is private and there is an admin logged in who is not the remote search user
-                || ( privacyLevel.equals( PrivacyLevelType.PRIVATE ) && currentUser != null && currentUser.getRoles().contains( getServiceAccountRole() ) && !isRemoteSearchUser( currentUser ) ) // user is a service account
+                || ( privacyLevel == PrivacyLevelType.PUBLIC ) // Data is public
+                || ( privacyLevel == PrivacyLevelType.SHARED && currentUser != null && !isRemoteSearchUser( currentUser ) )// data is accessible for registerd users and there is a user logged in who is not the remote admin
+                || ( privacyLevel == PrivacyLevelType.PRIVATE && currentUser != null && currentUser.getRoles().contains( getAdminRole() ) && !isRemoteSearchUser( currentUser ) )// data is private and there is an admin logged in who is not the remote search user
+                || ( privacyLevel == PrivacyLevelType.PRIVATE && currentUser != null && currentUser.getRoles().contains( getServiceAccountRole() ) && !isRemoteSearchUser( currentUser ) ) // user is a service account
                 || ( profile.isShared() && currentUser != null && currentUser.getRoles().contains( getAdminRole() ) && isRemoteSearchUser( currentUser ) ); // data is designated as remotely shared and there is an admin logged in who is the remote search user
     }
 
