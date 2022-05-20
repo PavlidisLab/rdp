333cbfffcde9bb005caff73e6caacae3c1f1d93b
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2022-05-20 11:46:16 -0700

    Move Serializable interface from Gene to GeneInfo
    It is only required for the UserGene association.

--- src/main/java/ubc/pavlab/rdp/model/Gene.java
+++ src/main/java/ubc/pavlab/rdp/model/Gene.java
@@ -17,7 +17,7 @@ import java.util.Date;
 @Setter
 @EqualsAndHashCode(of = { "geneId" })
 @ToString(of = { "geneId", "symbol", "taxon" })
-public abstract class Gene implements Serializable {
+public abstract class Gene {
 
     @Column(name = "gene_id")
     private int geneId;
