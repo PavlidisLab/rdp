d2691673dc1dfde74a678f1526348821dffa47da
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2022-07-10 16:57:14 -0700

    Explicitly use a ResourceLoader for loading GO and Uberon terms

--- src/test/java/ubc/pavlab/rdp/services/GOServiceImplTest.java
+++ src/test/java/ubc/pavlab/rdp/services/GOServiceImplTest.java
@@ -45,7 +45,7 @@ public class GOServiceImplTest {
             ApplicationSettings a = new ApplicationSettings();
             ApplicationSettings.CacheSettings cacheSettings = new ApplicationSettings.CacheSettings();
             cacheSettings.setEnabled( false );
-            cacheSettings.setTermFile( new ClassPathResource( "cache/go.obo" ) );
+            cacheSettings.setTermFile( "classpath:cache/go.obo" );
             cacheSettings.setAnnotationFile( new ClassPathResource( "cache/gene2go.gz" ) );
             a.setCache( cacheSettings );
             return a;
