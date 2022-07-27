7aca83f192aac5b153e82284c497c693c27868f4
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2022-07-27 14:35:27 -0700

    Fix Flyway migration with pre-1.5 checksums (fix #234)
    Flyway 3.2.1 (used on prior to the 1.5 series) computes checksum
    differently. We thus store pre and post 1.5 checksums to determine if a
    Flyway repair step is necessary before running migrations.

---
+++ src/main/java/ubc/pavlab/rdp/MigrationConfig.java
@@ -0,0 +1,72 @@
+package ubc.pavlab.rdp;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.apachecommons.CommonsLog;
+import org.flywaydb.core.api.MigrationInfo;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+/**
+ * Configuration related to the database migration, generally involving {@link org.flywaydb.core.Flyway}.
+ *
+ * @author poirigui
+ */
+@Configuration
+@CommonsLog
+public class MigrationConfig {
+
+    /**
+     * Expected migration information that are used to determine if a repair is necessary.
+     */
+    @RequiredArgsConstructor
+    private static class ExpectedMigrationInfo {
+        private final String version;
+        private final int pre15Checksum;
+        private final int post15Checksum;
+    }
+
+    private static final ExpectedMigrationInfo[] MIGRATION_META = {
+            new ExpectedMigrationInfo( "1.0.0", -330642568, 1889522940 ),
+            new ExpectedMigrationInfo( "1.3.2", 1109324745, 1109324745 ),
+            new ExpectedMigrationInfo( "1.4.0", 310023814, 1017485172 ),
+            new ExpectedMigrationInfo( "1.4.1", -1543189200, 1330781885 ),
+            new ExpectedMigrationInfo( "1.4.2", 1706447069, 253128706 ),
+            new ExpectedMigrationInfo( "1.4.3", 571489108, 571489108 ),
+            new ExpectedMigrationInfo( "1.4.6", 399726006, 504755998 ),
+            new ExpectedMigrationInfo( "1.4.11", 1536441374, 709491229 ),
+            new ExpectedMigrationInfo( "1.4.11.1", -1312864724, -907949910 )
+    };
+
+    /**
+     * Perform flyway.repair() to fix existing checksums.
+     */
+    @Bean
+    public FlywayMigrationStrategy flywayMigrationStrategy() {
+        return flyway -> {
+            MigrationInfo[] appliedMigrations = flyway.info().applied();
+            boolean repairNeeded = false;
+            for ( MigrationInfo appliedMigration : appliedMigrations ) {
+                ExpectedMigrationInfo expectedMigration = Arrays.stream( MIGRATION_META )
+                        .filter( mm -> mm.version.equals( appliedMigration.getVersion().getVersion() ) )
+                        .findAny()
+                        .orElse( null );
+                if ( expectedMigration != null &&
+                        appliedMigration.getChecksum() != null &&
+                        appliedMigration.getChecksum().equals( expectedMigration.pre15Checksum ) &&
+                        !appliedMigration.getChecksum().equals( expectedMigration.post15Checksum ) ) {
+                    log.warn( String.format( "Flyway migration %s is still pre-1.5, checksum will be bumped from %s to %s after repair.",
+                            appliedMigration.getVersion(), appliedMigration.getChecksum(), expectedMigration.post15Checksum ) );
+                    repairNeeded = true;
+                }
+            }
+            if ( repairNeeded ) {
+                log.warn( "Pre-1.5 migrations detected, Flyway repair will be performed." );
+                flyway.repair();
+            }
+            flyway.migrate();
+        };
+    }
+}
