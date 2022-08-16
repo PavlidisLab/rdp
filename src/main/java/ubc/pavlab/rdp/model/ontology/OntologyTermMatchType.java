21276c7bd53c2922bb9f93559f558b838badeb36
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2022-08-16 13:37:21 -0700

    Use title case for OntologyTermMatchType labels

--- src/main/java/ubc/pavlab/rdp/model/ontology/OntologyTermMatchType.java
+++ src/main/java/ubc/pavlab/rdp/model/ontology/OntologyTermMatchType.java
@@ -11,10 +11,10 @@ public enum OntologyTermMatchType implements MatchType {
     TERM_ID_EXACT( "Term ID", 0 ),
     ALT_ID_EXACT( "Term ID", 0 ),
 
-    TERM_NAME_EXACT( "Term name", 1 ),
-    TERM_NAME_STARTS_WITH( "Term name", 1 ),
-    TERM_NAME_CONTAINS( "Term name", 1 ),
-    TERM_NAME_MATCH( "Term name", 1 ),
+    TERM_NAME_EXACT( "Term Name", 1 ),
+    TERM_NAME_STARTS_WITH( "Term Name", 1 ),
+    TERM_NAME_CONTAINS( "Term Name", 1 ),
+    TERM_NAME_MATCH( "Term Name", 1 ),
 
     SYNONYM_EXACT( "Synonym", 2 ),
     SYNONYM_STARTS_WITH( "Synonym", 2 ),
