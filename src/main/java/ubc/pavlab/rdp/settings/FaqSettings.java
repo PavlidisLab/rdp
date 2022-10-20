615b51eb3c651bb44976a6dea9b07d5df3521b60
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2022-10-20 12:31:47 -0700

    Make entries in faq.properties translatable and order-preserving (fix #12)

--- src/main/java/ubc/pavlab/rdp/settings/FaqSettings.java
+++ src/main/java/ubc/pavlab/rdp/settings/FaqSettings.java
@@ -1,11 +1,18 @@
 package ubc.pavlab.rdp.settings;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.MessageSource;
+import org.springframework.context.MessageSourceResolvable;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import ubc.pavlab.rdp.util.Messages;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.SortedMap;
 
 /**
  * Created by mjacobson on 22/01/18.
@@ -16,7 +23,17 @@ import java.util.Map;
 @Data
 public class FaqSettings {
 
-    private Map<String, String> questions;
-    private Map<String, String> answers;
+    @Autowired
+    private MessageSource messageSource;
 
+    private LinkedHashMap<String, String> questions;
+    private LinkedHashMap<String, String> answers;
+
+    public MessageSourceResolvable getResolvableQuestion( String key ) {
+        return new DefaultMessageSourceResolvable( new String[]{ "rdp.faq.questions." + key }, new Object[]{ Messages.SHORTNAME }, questions.get( key ) );
+    }
+
+    public MessageSourceResolvable getResolvableAnswer( String key ) {
+        return new DefaultMessageSourceResolvable( new String[]{ "rdp.faq.answers." + key }, new Object[]{ Messages.SHORTNAME }, answers.get( key ) );
+    }
 }
