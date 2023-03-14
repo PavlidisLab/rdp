2dee47ec81b9b5dbbd102230c97f87d53e4d546a
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2023-03-14 12:04:32 -0700

    Revert JavaMailSender bean definition in EmailConfig shadowing the default Spring Boot-supplied one

--- src/main/java/ubc/pavlab/rdp/EmailConfig.java
+++ src/main/java/ubc/pavlab/rdp/EmailConfig.java
@@ -3,17 +3,10 @@ package ubc.pavlab.rdp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.AsyncTaskExecutor;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 public class EmailConfig {
-
-    @Bean
-    public JavaMailSender emailSender() {
-        return new JavaMailSenderImpl();
-    }
 
     @Bean
     public AsyncTaskExecutor emailTaskExecutor() {
