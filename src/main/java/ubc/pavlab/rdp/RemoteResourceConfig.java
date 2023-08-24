ef34305a81d40973f04960617fc22df52fdde078
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2023-08-24 10:19:16 -0700

    Add a User-Agent header when requesting partner APIs (fix #369)

--- src/main/java/ubc/pavlab/rdp/RemoteResourceConfig.java
+++ src/main/java/ubc/pavlab/rdp/RemoteResourceConfig.java
@@ -1,6 +1,7 @@
 package ubc.pavlab.rdp;
 
 import lombok.extern.apachecommons.CommonsLog;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
@@ -8,14 +9,24 @@ import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.AsyncRestTemplate;
 import ubc.pavlab.rdp.settings.ApplicationSettings;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+
 @CommonsLog
 @Configuration
 public class RemoteResourceConfig {
 
     @Bean
-    public AsyncRestTemplate remoteResourceRestTemplate( ApplicationSettings applicationSettings ) {
-        SimpleClientHttpRequestFactory httpRequestFactory = new SimpleClientHttpRequestFactory();
+    public AsyncRestTemplate remoteResourceRestTemplate( ApplicationSettings applicationSettings, BuildProperties buildProperties ) {
+        SimpleClientHttpRequestFactory httpRequestFactory = new SimpleClientHttpRequestFactory() {
+            @Override
+            protected void prepareConnection( HttpURLConnection connection, String httpMethod ) throws IOException {
+                super.prepareConnection( connection, httpMethod );
+                connection.setRequestProperty( "User-Agent", buildProperties.getArtifact() + "/" + buildProperties.getVersion() );
+            }
+        };
         httpRequestFactory.setTaskExecutor( new SimpleAsyncTaskExecutor() );
+        //noinspection deprecation
         if ( applicationSettings.getIsearch().getRequestTimeout() != null ) {
             log.warn( "The 'rdp.settings.isearch.request-timeout' configuration is deprecated, use 'rdp.settings.isearch.connect-timeout' and 'rdp.settings.isearch.read-timeout' instead." );
             httpRequestFactory.setConnectTimeout( 1000 );
